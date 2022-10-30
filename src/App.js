2802fac5381531d0d5b7e97558f1e0b15a9dd3c9
Memory-Wabwile <memorywabwile@gmail.com>
2022-10-30 13:13:48 +0300

    create the twitter button and style it

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className='container-fluid'>
+    <div className='container'>
     <Header />
     <Home />
     </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './Home.css'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
   return (
-    <div>
-      <Button variant="secondary" > Twiter Link</Button>{''}
+    <div  >
+      <Button id="twitter" className='class-btns' > <a href='https://twitter.com/__nimmoh__' target="_blank">Twiter Link</a></Button>
     </div>
   )
 }
