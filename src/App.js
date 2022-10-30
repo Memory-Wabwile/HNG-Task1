b4902413e061d561e67cd7ebdb22437d41f53d50
Memory-Wabwile <memorywabwile@gmail.com>
2022-10-30 12:14:16 +0300

    create the assets folder for storing the images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import Home from './Home/Home';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
-    <Home/>
+    <div className='container-fluid'>
+    <Header />
+    <Home />
+    </div>
   );
 }
 
