b62d2c9486ee9953b2b9fc25e3adf4dcafa7965d
Memory-Wabwile <memorywabwile@gmail.com>
2022-10-30 12:27:50 +0300

    render the profile picture and name  on the page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,9 @@ import React from 'react'
 function Header() {
   return (
     <div>
+      <img id='profile_image' src='nimo2.jpeg' alt='profile-image'/>
+
+      <h3>Memory Wabwile</h3>   
         
     </div>
   )
