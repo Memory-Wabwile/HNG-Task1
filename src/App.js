08aa9bc4e013464539c492474b327f7496eb7afc
Memory-Wabwile <memorywabwile@gmail.com>
2022-10-30 16:28:37 +0300

    create the footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 
 function App() {
@@ -8,8 +9,10 @@ function App() {
     <div className='container'>
     <Header />
     <Home />
+    <Footer/>
     </div>
   );
 }
+
 
 export default App;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,15 +5,22 @@ import './Home.css'
 
 function Home() {
   return (
-    <div  >
-      <Button id="twitter"  > <a href='https://twitter.com/__nimmoh__' target="_blank">Twiter Link</a></Button>
+    <div >
+       <a href='https://twitter.com/__nimmoh__' target="_blank"><Button id="twitter"  >Twiter Link</Button></a>
       <Button id="btn__zuri" > <a href='https://training.zuri.team/' target="_blank">Zuri Team</a></Button>
       <Button id="books"  > <a href='http://books.zuri.team ' target="_blank">Zuri Books</a></Button>
-      <Button id="book__python"  > <a href='https://twitter.com/__nimmoh__' target="_blank">Python Books</a></Button>
+      <Button id="book__python"  > <a href='https://books.zuri.team/python-for-beginners?ref_id=Memory-Wabwile' target="_blank">Python Books</a></Button>
       <Button id="pitch"  > <a href='https://background.zuri.team' target="_blank">Background Check for Coders</a></Button>
       <Button id="book__design"  > <a href='https://books.zuri.team/design-rules' target="_blank">Design Books</a></Button>
       
+      <div className='logos'>
+      <a href='http://books.zuri.team ' target="_blank" ><img id='slack' src='slackk-logo.png' alt='slack logo'/></a>
+      <a href='http://books.zuri.team ' target="_blank"><img id='github' src='githublogo.png' alt='github logo'/></a>
+      </div>
     </div>
+
+
+
   )
 }
 
